20317524377769c480a5cec3c85d556ac1a36414
jmorales113 <jmorales113@gmail.com>
2020-08-06 13:36:25 -0700

    Refactored code & added Babel/Webpack

---
+++ src/edit.js
@@ -0,0 +1,35 @@
+import { initializeEditPage, generateLastEdited } from "./views.js"
+import { updateNote, removeNote } from "./notes.js"
+
+const titleEl = document.querySelector("#note-title")
+const bodyEl = document.querySelector("#note-body")
+const removeEl = document.querySelector("#remove-note")
+const dateEl = document.querySelector("#last-edited")
+const noteId = location.hash.substring(1)
+
+initializeEditPage(noteId)
+
+titleEl.addEventListener("input", (e) => {
+    const note = updateNote(noteId, {
+        title: e.target.value
+    })
+    dateEl.textContent = generateLastEdited(note.updatedAt)
+})
+
+bodyEl.addEventListener("input", (e) => {
+    const note = updateNote(noteId, {
+        body: e.target.value
+    })
+    dateEl.textContent = generateLastEdited(note.updatedAt)
+})
+
+removeEl.addEventListener("click", (e) => {
+    removeNote(noteId)
+    location.assign("./index.html")
+})
+
+window.addEventListener("storage", (e) => {
+    if (e.key === "notes") {
+        initializeEditPage(noteId)
+    }
+})
